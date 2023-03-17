2cffa1cb8aa028de12490fa7e5e7a0a4452f2a9e
Pratik Ramteke <pratikramteke222@gmail.com>
2023-03-17 15:56:26 +0530

    redux toolkit added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo } from './features/todoSlice'
 
 function App() {
-
+  const count = useSelector((state) => state);
+ const dispatch=  useDispatch()
   const [input,setInput] =useState('')
   return (
+    <>
     <div className='flex justify-center my-10'>
-      <form className=''>
         <input className='border-2 border-black p-2 m-1' type="text" placeholder='Enter Task' onChange={e => setInput(e.target
         .value)} />
-        <button className='border-2 border-black p-2' disabled={input===""}>ADD TASK</button>
-      </form>
+      <button className='border-2 border-black p-2' disabled={input === ""} onClick={() => dispatch(addTodo(input))}>ADD TASK</button>
     </div>
+      <div>{ count}</div>
+    </>
   )
 }
 
